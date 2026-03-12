83bcbbf63b5093bf3d73d453579dba2a2abdd4e6
mohammedali2323 <161701511+mohammedali2323@users.noreply.github.com>
2026-03-13 02:43:07 +0530

    Add translations for traffic signs in multiple languages

---
+++ translator.py
@@ -0,0 +1,19 @@
+signs = {
+
+"stop":{
+"English":"Stop",
+"Kannada":"ನಿಲ್ಲಿ",
+"Tamil":"நிறுத்து",
+"Telugu":"ఆపు",
+"Malayalam":"നിർത്തുക"
+},
+
+"traffic light":{
+"English":"Traffic Signal",
+"Kannada":"ಟ್ರಾಫಿಕ್ ಸಿಗ್ನಲ್",
+"Tamil":"போக்குவரத்து சிக்னல்",
+"Telugu":"ట్రాఫిక్ సిగ్నల్",
+"Malayalam":"ട്രാഫിക് സിഗ്നൽ"
+}
+
+}
